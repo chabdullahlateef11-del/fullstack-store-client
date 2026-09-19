4b871cbfe13a0d2d766baf0818e2fdca20bd9e3c
abdullah <chabdullahlateef11@gmail.com>
2026-09-19 15:12:47 +0500

    Fix axios base URL

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "http://localhost:5050/api" });
+const API = axios.create({ baseURL: "https://fullstack-store-server-production.up.railway.app/api" });
 
 API.interceptors.request.use((req) => {
   const token = localStorage.getItem("token");
